b1cdd3ae852b4e2a04add013e503c0c91f020889
Jeff Parrish <jeffhparrish@gmail.com>
2017-08-04 23:06:26 -0700

    working though random forest

--- jg/rfpython.go
+++ jg/rfpython.go
@@ -3,18 +3,13 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"strconv"
 	"strings"
 )
 
-type Dataset struct {
-	Coords []float32
-	Val    string
-}
-
-// Load a CSV File
-// last column is a string value
-func load_csv(filename string) (dataset []*Dataset) {
+// Load a CSV File - last column is a string value
+func load_csv(filename string) (dataset []DataRow) {
 	buf, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
@@ -26,15 +21,66 @@ func load_csv(filename string) (dataset []*Dataset) {
 		parsedRow := make([]float32, len(cols)-1)
 		for c := 0; c < len(cols)-1; c++ {
 			f64, err := strconv.ParseFloat(cols[c], 32)
-			if err != nil {
-				panic(err)
-			}
+			if err != nil { panic(err) }
 			parsedRow[c] = float32(f64)
 		}
-		d := &Dataset{Coords: parsedRow, Val: cols[len(cols)-1]}
+		d := DataRow{Coords: parsedRow, Val: cols[len(cols)-1]}
 		dataset = append(dataset, d)
 	}
 	return dataset
+}
+
+// Split a dataset into k folds
+func cross_validation_split(dataset []DataRow, n_folds int) (dataset_split []DataRow) {
+	dataset_copy := dataset[0:]
+	fold_size := len(dataset) / n_folds
+	for i := 0; i < n_folds; i++ {
+		var fold []DataRow
+		for {
+			// take a random index from the dataset, remove it, and add it to our
+			// fold list, which gets appended to the dataset_split
+			index := rand.Intn(len(dataset_copy))
+			item := dataset_copy[index]
+			dataset_copy = append(dataset_copy[:index], dataset_copy[index+1:]...) // delete
+			fold = append(fold, item)
+			if len(fold) < fold_size {
+				break
+			}
+		}
+		dataset_split = append(dataset_split, fold...)
+	}
+	return dataset_split
+}
+
+// Calculate accuracy percentage
+func accuracy_metric(actual string, predicted string) float32 {
+	var correct float32 = 0.0
+	lenActual := len(actual)
+	for i := 0; i < lenActual; i++ {
+		if actual[i] == predicted[i] {
+			correct++
+		}
+	}
+	return 100 * correct / float32(lenActual)
+}
+
+//Evaluate an algorithm using a cross validation split
+func evaluate_algorithm(dataset []DataRow, n_folds int, args ...interface{}) {
+	folds := cross_validation_split(dataset, n_folds)
+	var scores []DataRow
+	for ix, fold := range folds {
+		train_set := append(folds[:ix], folds[ix+1:]...) // all but this fold
+		train_set = sum(train_set) // summed each value so there is one item per DataRow
+		var test_set []DataRow
+		for _, row := range fold.Coords {
+			row_copy := row
+			test_set = append(test_set, row_copy)
+		}
+		predicted = random_forest(train_set, test_set, *args)
+		actual = [row[-1] for row in fold]
+		accuracy = accuracy_metric(actual, predicted)
+		scores.append(accuracy)
+	}
 }
 
 func main() {
