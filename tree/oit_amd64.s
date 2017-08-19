bffea6b6f4b05c2b3603c26761824dec7ab0669e
Jeff Parrish <jeffhparrish@gmail.com>
2017-08-19 14:10:25 -0700

    revert test code in assembler oneIfTrue for clarity

--- tree/oit_amd64.s
+++ tree/oit_amd64.s
@@ -1,8 +1,8 @@
 // func oneIfTrue(x, y float32) float32
 TEXT ·oneIfTrue(SB),$0
-    MOVQ 0.0, ret+8(FP) // default 0 to the return value
-    MOVQ val1+0(FP), BX // put first arg in the BX register
-    SUBQ BX, val2+4(FP) // subtract the second arg from the first
+    MOVQ 0x0000, ret+8(FP) // default 0 to the return value
+    MOVQ x+0(FP), BX // put first arg in the BX register
+    SUBQ BX, y+4(FP) // subtract the second arg from the first
     CMPQ BX, 0x0000 // see if the register is now 0
     JEQ ifeq // when equal, change the return value
     RET
