75acc11a14b0239e63995e355f48da42bba90f45
Jeff Parrish <jeffhparrish@gmail.com>
2017-08-19 15:18:17 -0700

    Update oit_amd64.s

--- tree/oit_amd64.s
+++ tree/oit_amd64.s
@@ -1,10 +1,10 @@
 // func oneIfTrue(x, y float32) float32
 TEXT ·oneIfTrue(SB),$0
-    MOVQ 0x0000, ret+8(FP) // default 0 to the return value
+    MOVQ 0x00000000, ret+8(FP) // default 0 to the return value
     MOVQ x+0(FP), BX // put first arg in the BX register
-    SUBQ BX, y+4(FP) // subtract the second arg from the first
-    CMPQ BX, 0x0000 // see if the register is now 0
+    SUBQ BX, y+8(FP) // subtract the second arg from the first
+    CMPQ BX, 0x00000000 // see if the register is now 0
     JEQ ifeq // when equal, change the return value
     RET
 ifeq:
-    MOVQ 0x0001, ret+8(FP) // move 1 to the return value
+    MOVQ 0x3f800000, ret+16(FP) // move 1 to the return value
