6984c5dc86d854da3f4eac80bf5185ec9efaf3a0
Jeff Parrish <jeffhparrish@gmail.com>
2017-08-19 22:35:50 -0700

    fix assembly 4 byte float offsets

--- tree/oit_amd64.s
+++ tree/oit_amd64.s
@@ -6,9 +6,9 @@
 TEXT ·oneIfTrue(SB),$8
     MOVQ $0, ret+8(FP) // default 0 to the return value
     MOVQ x+0(FP), BX // put first arg in the BX register
-    SUBQ BX, y+8(FP) // subtract the second arg from the first
+    SUBQ BX, y+4(FP) // subtract the second arg from the first
     CMPQ BX, $0 // see if the register is now 0
     JEQ ifeq // when equal, change the return value
     RET
 ifeq:
-    MOVQ $1, ret+16(FP) // move 1 to the return value
+    MOVQ $1, ret+8(FP) // move 1 to the return value
